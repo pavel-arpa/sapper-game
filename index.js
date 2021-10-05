a5256cf5a03485ff072e83c3a808580d87ced5e0
Pavel Arepev <protocoltype7@gmail.com>
2021-10-05 18:08:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import start from "./components/game/start.js";
 import preloadImages from "./optimizeData/preloadImages.js";
 
-document.innerHtml = "<div style='display:flex; height: 100%; align-items: center; justify-content: center; font-size: 50px;'>Downloading</div>";
+document.querySelector('body').innerHTML = "<div style='display:flex; height: 100%; align-items: center; justify-content: center; font-size: 50px;'>Downloading</div>";
 (async () => {
     await preloadImages(["./public/bomb.jpg", "./public/desert.png", "./public/explosion.png", "./public/flag.png", "./public/grass.jpg", "./public/grass__block.png"])
     await start()
