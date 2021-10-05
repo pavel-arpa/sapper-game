49aa67559ae85378668fc383ec905295474d1a5c
Pavel Arepev <protocoltype7@gmail.com>
2021-10-05 18:13:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import start from "./components/game/start.js";
 import preloadImages from "./optimizeData/preloadImages.js";
 
 (async () => {
-    await preloadImages(["./public/bomb.jpg", "./public/desert.png", "./public/explosion.png", "./public/flag.png", "./public/grass.jpg", "./public/grass__block.png"])
+    // await preloadImages(["./public/bomb.jpg", "./public/desert.png", "./public/explosion.png", "./public/flag.png", "./public/grass.jpg", "./public/grass__block.png"])
     await start()
 })()
